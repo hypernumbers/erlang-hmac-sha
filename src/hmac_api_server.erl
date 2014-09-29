7574577ab23f0a9b48d7e04c5a929a78a12f78fb
James Casey <james@opscode.com>
2014-09-28 19:40:53 -0700

    add early failure in wm_authenticate if there are missing headers

--- src/hmac_api_server.erl
+++ src/hmac_api_server.erl
@@ -12,7 +12,7 @@
 -spec wm_authenticate(Req :: #wm_reqdata{},
                       State :: any(),
                       PrivateKeyFun :: fun((string()) -> string() | undefined))
- -> {true | string(), #wm_reqdata{}, any()}.
+ -> {true | string(), #wm_reqdata{}, any()} | {error, binary()}.
 wm_authenticate(Req, State, PrivateKeyFun) ->
     wm_authenticate(Req, State, PrivateKeyFun, #hmac_config{}).
 
@@ -20,33 +20,61 @@ wm_authenticate(Req, State, PrivateKeyFun) ->
                       State :: any(),
                       PrivateKeyFun :: fun((string()) -> string() | undefined),
                       Config :: #hmac_config{})
- -> {true | string(), #wm_reqdata{}, any()}.
+ -> {true | string(), #wm_reqdata{}, any()} | {error, binary()}.
 wm_authenticate(Req, State, PrivateKeyFun, Config) ->
-    Method = wrq:method(Req),
-    Path = wrq:path(Req),
-    Headers = hma_util:normalise(wrq:req_headers(Req)),
-    ContentMD5 = wrq:get_req_header("content-md5", Req),
-    ContentType = wrq:get_req_header("content-type", Req),
-    Date = wrq:get_req_header("date", Req),
-    Authorization = wrq:get_req_header("authorization", Req),
-    {_Schema, PublicKey, _Sig} = hmac_api_lib:parse_authorization_header(Authorization),
+    case assert_headers(Config, Req) of
+        {error, _Error} = Reason ->
+            Reason;
+        {Authorization, Date} ->
+            Method = wrq:method(Req),
+            Path = wrq:path(Req),
+            Headers = hma_util:normalise(mochiweb_headers:to_list(wrq:req_headers(Req))),
+            ContentMD5 = wrq:get_req_header("Content-MD5", Req),
+            ContentType = wrq:get_req_header("Content-Type", Req),
 
-    case PrivateKeyFun(PublicKey) of
-        undefined ->
-            {"HMAC", Req, State};
-        PrivateKey ->
-            Signature = #hmac_signature{config = Config,
-                                        method = Method,
-                                        contentmd5 = ContentMD5,
-                                        contenttype = ContentType,
-                                        date = Date,
-                                        headers = Headers,
-                                        resource = Path},
-            case hmac_api_lib:validate(PrivateKey, PublicKey, Authorization, Signature) of
-                match ->
-                    {true, Req, State};
-                no_match ->
-                    {"HMAC", Req, State}
+            {_Schema, PublicKey, _Sig} = hmac_api_lib:parse_authorization_header(Authorization),
+
+            case PrivateKeyFun(PublicKey) of
+                undefined ->
+                    {"HMAC", Req, State};
+                PrivateKey ->
+                    Signature = #hmac_signature{config = Config,
+                                                method = Method,
+                                                contentmd5 = ContentMD5,
+                                                contenttype = ContentType,
+                                                date = Date,
+                                                headers = Headers,
+                                                resource = Path},
+                    case hmac_api_lib:validate(PrivateKey, PublicKey, Authorization, Signature) of
+                        match ->
+                            {true, Req, State};
+                        no_match ->
+                            {"HMAC", Req, State}
+                    end
             end
+    end.
+
+%% Check that all the appropriate headers are present in the Request
+%% We require
+%% 1. Authorization
+%% 2. EITHER Date or #hmac_config{date_header} (normally X-HMAC-Date)
+assert_headers(Config, Req) ->
+    case wrq:get_req_header("Authorization", Req) of
+        undefined ->
+            {error, <<"Missing Authorization header">>};
+        Auth ->
+            case date_header(Config, Req) of
+                undefined ->
+                    {error, <<"Missing Date header">>};
+                Date ->
+                    {Auth, Date}
+            end
+    end.
+
+date_header(#hmac_config{header_prefix = Prefix}, Req) ->
+    DateHeader = string:to_upper(Prefix) ++ "Date",
+    case wrq:get_req_header(DateHeader, Req) of
+        undefined -> wrq:get_req_header("Date", Req);
+        Date -> Date
     end.
 
--- test/hmac_api_server_tests.erl
+++ test/hmac_api_server_tests.erl
@@ -25,7 +25,7 @@ wm_test_() ->
     ContentMD5 = "",
     ContentType = "",
     Date = "Tue, 27 Mar 2007 21:20:26 +0000",
-    Headers = [{"x-amz-date", Date}],
+    Headers = mochiweb_headers:from_list([{"x-amz-date", Date}]),
 
     Authorization = "AWS " ++ ?PUBLIC_KEY ++ ":k3nL7gH3+PadhTEVn5Ip83xlYzk=",
     {foreach,
@@ -35,10 +35,11 @@ wm_test_() ->
       meck:expect(wrq, req_headers, fun(req) -> Headers end),
       meck:expect(wrq, get_req_header, fun(Name, req) ->
                                                case Name of
-                                                   "content-md5" -> ContentMD5;
-                                                   "content-type" -> ContentType;
-                                                   "date" -> Date;
-                                                   "authorization" -> Authorization;
+                                                   "Content-MD5" -> ContentMD5;
+                                                   "Content-Type" -> ContentType;
+                                                   "X-AMZ-Date" -> undefined;
+                                                   "Date" -> Date;
+                                                   "Authorization" -> Authorization;
                                                    Else ->
                                                        ?debugVal("Unknown header: " ++ Else),
                                                        ?assert(false)
@@ -51,7 +52,7 @@ wm_test_() ->
         {"validate an authorization header",
          fun() ->
                  Got = hmac_api_server:wm_authenticate(req, state, fun private_key/1, hmac_aws:config()),
-                 ?assertEqual(Got, {true, req, state})
+                 ?assertEqual({true, req, state}, Got)
          end
         },
         {"wrong private key",
@@ -59,7 +60,7 @@ wm_test_() ->
                  %% This is not the key we're looking for
                  PrivateKeyFun = fun(_Public) -> "theyarenotthedroidsyouarelookingfor" end,
                  Got = hmac_api_server:wm_authenticate(req, state, PrivateKeyFun, hmac_aws:config()),
-                 ?assertEqual(Got, {"HMAC", req, state})
+                 ?assertEqual({"HMAC", req, state}, Got)
          end
         },
         {"unknown private key",
@@ -67,9 +68,103 @@ wm_test_() ->
                  %% know not a key
                  PrivateKeyFun = fun(_Public) -> undefined end,
                  Got = hmac_api_server:wm_authenticate(req, state, PrivateKeyFun, hmac_aws:config()),
-                 ?assertEqual(Got, {"HMAC", req, state})
+                 ?assertEqual({"HMAC", req, state}, Got)
          end
         }
+     ]
+    }.
+
+wm_missing_headers_test_() ->
+    MockedModules = [wrq],
+    URL = "http://exAMPLE.Com:90/johnsmith/photos/puppy.jpg",
+    Method = delete,
+    ContentMD5 = "",
+    ContentType = "",
+    Date = "Tue, 27 Mar 2007 21:20:26 +0000",
+    Headers = mochiweb_headers:from_list([{"x-amz-date", Date}]),
+
+    Authorization = "AWS " ++ ?PUBLIC_KEY ++ ":k3nL7gH3+PadhTEVn5Ip83xlYzk=",
+    {foreach,
+     fun() -> setup(MockedModules),
+      meck:expect(wrq, method, fun(req) -> Method end),
+      meck:expect(wrq, path, fun(req) -> URL end),
+      meck:expect(wrq, req_headers, fun(req) -> Headers end)
+     end,
+     fun(_) -> cleanup(MockedModules) end,
+     [
+        {"missing both Date headers",
+         fun() ->
+                 meck:expect(wrq, get_req_header, fun(Name, req) ->
+                                                          case Name of
+                                                              "Content-MD5" -> ContentMD5;
+                                                              "Content-Type" -> ContentType;
+                                                              "X-AMZ-Date" -> undefined;
+                                                              "Date" -> undefined;
+                                                              "Authorization" -> Authorization;
+                                                          Else ->
+                                                              ?debugVal("Unknown header: " ++ Else),
+                                                              ?assert(false)
+                                               end
+                                       end),
+                 Got = hmac_api_server:wm_authenticate(req, state, fun private_key/1, hmac_aws:config()),
+                 ?assertEqual({error, <<"Missing Date header">>}, Got)
+         end
+        },
+        {"missing Date headers",
+         fun() ->
+                 meck:expect(wrq, get_req_header, fun(Name, req) ->
+                                                          case Name of
+                                                              "Content-MD5" -> ContentMD5;
+                                                              "Content-Type" -> ContentType;
+                                                              "X-AMZ-Date" -> Date;
+                                                              "Date" -> undefined;
+                                                              "Authorization" -> Authorization;
+                                                          Else ->
+                                                              ?debugVal("Unknown header: " ++ Else),
+                                                              ?assert(false)
+                                               end
+                                       end),
+                 Got = hmac_api_server:wm_authenticate(req, state, fun private_key/1, hmac_aws:config()),
+                 ?assertEqual({true, req, state}, Got)
+         end
+        },
+        {"missing X-AMZ-Date headers",
+         fun() ->
+                 meck:expect(wrq, get_req_header, fun(Name, req) ->
+                                                          case Name of
+                                                              "Content-MD5" -> ContentMD5;
+                                                              "Content-Type" -> ContentType;
+                                                              "X-AMZ-Date" -> undefined;
+                                                              "Date" -> Date;
+                                                              "Authorization" -> Authorization;
+                                                          Else ->
+                                                              ?debugVal("Unknown header: " ++ Else),
+                                                              ?assert(false)
+                                               end
+                                       end),
+                 Got = hmac_api_server:wm_authenticate(req, state, fun private_key/1, hmac_aws:config()),
+                 ?assertEqual({true, req, state}, Got)
+         end
+        },
+        {"missing Authorization header",
+         fun() ->
+                 meck:expect(wrq, get_req_header, fun(Name, req) ->
+                                                          case Name of
+                                                              "Content-MD5" -> ContentMD5;
+                                                              "Content-Type" -> ContentType;
+                                                              "X-AMZ-Date" -> Date;
+                                                              "Date" -> Date;
+                                                              "Authorization" -> undefined;
+                                                          Else ->
+                                                              ?debugVal("Unknown header: " ++ Else),
+                                                              ?assert(false)
+                                               end
+                                       end),
+                 Got = hmac_api_server:wm_authenticate(req, state, fun private_key/1, hmac_aws:config()),
+                 ?assertEqual({error, <<"Missing Authorization header">>}, Got)
+         end
+        }
+
      ]
     }.
 
